609bc8a35fc93cd528a98e0b0f42aed7c802d5ad
Irving Delgado <rvngdelgado@gmail.com>
2022-05-19 21:36:16 -0500

    Completed server, tables, migration

---
+++ data/migrations/20220520004253_create_tables.js
@@ -0,0 +1,53 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.createTable('projects', tbl => {
+        tbl.increments('project_id')
+        tbl.varchar('project_name').notNullable()    
+        tbl.varchar('project_description')
+        tbl.boolean('project_completed')
+    })
+    await knex.schema.createTable('resources', tbl => {
+        tbl.increments('resource_id')
+        tbl.varchar('resource_name').notNullable().unique()
+        tbl.varchar('resource_description')
+
+    })
+    await knex.schema.createTable('tasks', tbl => {
+        tbl.increments('task_id')
+        tbl.varchar('task_description').notNullable()    
+        tbl.varchar('task_notes')
+        tbl.boolean('task_completed')
+        tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')        
+    })
+    await knex.schema.createTable('project_resources', tbl => {
+        tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        tbl.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id')
+        .inTable('resources')
+        tbl.primary(['project_id', 'resource_id'])
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('project_resources')
+    await knex.schema.dropTableIfExists('tasks')
+    await knex.schema.dropTableIfExists('resources')
+    await knex.schema.dropTableIfExists('projects')    
+};
